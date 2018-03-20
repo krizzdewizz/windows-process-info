ea4f1a4cc5c83d4feedddf64a858958ba05f644d
Christian Oetterli <christian.oetterli@bison-group.com>
2018-03-20 01:09:31 +0100

    trim cwd, title

--- src/worker.cc
+++ src/worker.cc
@@ -1,5 +1,17 @@
 #include "worker.h"
 
+string trim(string s) {
+	auto str = s.c_str();
+	// Trim leading non-letters
+	while (!isalnum(*str)) str++;
+
+	// Trim trailing non-letters
+	auto end = str + strlen(str) - 1;
+	while (end > str && !isalnum(*end)) end--;
+
+	return string(str, end + 1);
+}
+
 Worker::Worker(DWORD pid, Nan::Callback *callback)
     : AsyncWorker(callback),
       pid(pid)
@@ -21,8 +33,8 @@ void Worker::HandleOKCallback()
   v8::Local<v8::Object> object = Nan::New<v8::Object>();
   if (error.empty())
   {
-    Nan::Set(object, Nan::New<v8::String>("cwd").ToLocalChecked(), Nan::New<v8::String>(cwd.c_str()).ToLocalChecked());
-    Nan::Set(object, Nan::New<v8::String>("title").ToLocalChecked(), Nan::New<v8::String>(title.c_str()).ToLocalChecked());
+    Nan::Set(object, Nan::New<v8::String>("cwd").ToLocalChecked(), Nan::New<v8::String>(trim(cwd).c_str()).ToLocalChecked());
+    Nan::Set(object, Nan::New<v8::String>("title").ToLocalChecked(), Nan::New<v8::String>(trim(title).c_str()).ToLocalChecked());
     Nan::Set(object, Nan::New<v8::String>("commandLine").ToLocalChecked(), Nan::New<v8::String>(commandLine.c_str()).ToLocalChecked());
   }
   else
